72e4b5bffdee81b8f87943b894aee7e6fffc344f
dumin199101 <dumin199101@163.com>
2017-08-10 23:29:26 +0800

    系统管理完成

--- application/admin/controller/System.php
+++ application/admin/controller/System.php
@@ -18,14 +18,24 @@ use app\admin\logic\GoodsLogic;
 use think\db;
 use think\Cache;
 class System extends Base{
-	
+
+	/**
+	 * 清空系统缓存
+	 */
+	public function cleanCache(){
+		delFile(RUNTIME_PATH);
+		Cache::clear();
+		$this->success("操作完成!!!",U('Admin/Admin/index'));
+		exit();
+	}
+
 	/*
 	 * 配置入口
 	 */
 	public function index()
 	{          
 		/*配置列表*/
-		$group_list = array('shop_info'=>'网站信息','basic'=>'基本设置','sms'=>'短信设置','shopping'=>'购物流程设置','smtp'=>'邮件设置','water'=>'水印设置','distribut'=>'分销设置');		
+		$group_list = array('shop_info'=>'网站信息');
 		$this->assign('group_list',$group_list);
 		$inc_type =  I('get.inc_type','shop_info');
 		$this->assign('inc_type',$inc_type);
@@ -39,7 +49,6 @@ class System extends Base{
 			$this->assign('area',$area);
 		}
 		$this->assign('config',$config);//当前配置项
-                //C('TOKEN_ON',false);
 		return $this->fetch($inc_type);
 	}
 	
@@ -50,7 +59,6 @@ class System extends Base{
 	{
 		$param = I('post.');
 		$inc_type = $param['inc_type'];
-		//unset($param['__hash__']);
 		unset($param['inc_type']);
 		tpCache($inc_type,$param);
 		$this->success("操作成功",U('System/index',array('inc_type'=>$inc_type)));
@@ -141,15 +149,7 @@ class System extends Base{
 	}
 
         
-        /**
-         * 清空系统缓存
-         */
-        public function cleanCache(){
-			delFile(RUNTIME_PATH);
-			Cache::clear(); 
-            $this->success("操作完成!!!",U('Admin/Admin/index'));
-            exit();
-        }
+
 	    
     /**
      * 清空静态商品页面缓存
