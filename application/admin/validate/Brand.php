08a20564ef758f97be383e380848e84c064510d5
lieyan123091 <1766266374@qq.com>
2017-08-30 15:59:52 +0800

    后台测试数据录入及数据库字段优化

--- application/admin/validate/Brand.php
+++ application/admin/validate/Brand.php
@@ -8,15 +8,19 @@ class Brand extends Validate
 
     protected $rule=[
         ['name' ,'require'],
+        ['title','require'],
+        ['attach','require'],
         ['url' ,'url'],
         ['sort','number'],
-        ['desc' ,'max:200']
+        ['desc' ,'max:500']
     ];
     protected $message = [
-        'name.require'      => '品牌名称必须',
+        'title.require'    => '品牌英文名称必须',
+        'attach.require'   => '品牌附加信息必须',
+        'name.require'      => '品牌中文名称必须',
         'url.url'           => '品牌地址不是有效的URL地址',
         'sort.number'       => '排序必须是数字',
-        'desc.max'          => '品牌描述不得大于200个字节'
+        'desc.max'          => '品牌描述不得大于500个字节'
     ];
 
 
