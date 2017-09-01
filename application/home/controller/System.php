282220ddfa1daf869db1441f3abdab6fcc5574ea
lieyan123091 <1766266374@qq.com>
2017-09-01 18:17:59 +0800

    关于君太MVC数据交互

--- application/home/controller/System.php
+++ application/home/controller/System.php
@@ -13,6 +13,9 @@
 namespace app\home\controller;
 
 
+use think\Request;
+use think\Db;
+
 class System extends Base
 {
     /**
@@ -21,6 +24,24 @@ class System extends Base
      */
     public function about()
     {
+        //全屏banner
+        $banner_list = S('Pos:system:banner_list');
+        if(empty($banner_list))
+        {
+            $banner_list = Db::name('banner')->alias('a')
+                ->field('a.banner_link,a.banner_code,a.target')
+                ->join('__BANNER_POSITION__ b','a.pid=b.position_id')
+                ->where('a.enabled',1)
+                ->where('a.start_time','<',time())
+                ->where('a.end_time','>',time())
+                ->where('b.position_name','like','%关于君太页%')
+                ->order('a.orderby desc')
+                ->select();
+            S('Pos:system:banner_list',$banner_list,JT_CACHE_TIME);
+        }
+        $position = Request::instance()->controller();
+        $this->assign('position',$position);
+        $this->assign('banner_list',$banner_list);
         return $this->fetch();
     }
 
