f94ff25c6a70ecf8dc4b346aa72186487bcc6664
lieyan123091 <1766266374@qq.com>
2017-09-04 14:56:26 +0800

    兼容响应式布局品牌字母筛选样式

--- application/home/config.php
+++ application/home/config.php
@@ -6,7 +6,7 @@ $home_config = [
 	//默认错误跳转对应的模板文件
 	'dispatch_error_tmpl' => 'public:dispatch_jump',
 	//默认成功跳转对应的模板文件
-	'dispatch_success_tmpl' => 'public:dispatch_jump', 
+	'dispatch_success_tmpl' => 'public:dispatch_jump',
 ];
 
 $html_config = include_once 'html.php';
