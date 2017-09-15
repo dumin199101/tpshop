b6b0a1b530ffd94ff5d77de4d71e025c8feb4524
dumin199101 <dumin199101@163.com>
2017-09-15 22:28:20 +0800

    后台君太VIP管理

--- application/admin/controller/Other.php
+++ application/admin/controller/Other.php
@@ -67,12 +67,59 @@ class Other extends Base
     public function vipUpdate()
     {
         if(request()->isPost()){
+            $result = Db::name('web_static')->where('id',2)->whereOr('pid',2)->select();
+            foreach($result as $val){
+                $temp[$val['name']] = $val['content'];
+            }
             $data = input('post.');
-            Db::name('web_static')->where('id',2)->save($data);
+            //看名称是否有变化
+            foreach($result as $val){
+                if(empty($val['title']))
+                    continue;
+                $tmp_name[$val['name']] = $val['title'];
+            }
+            foreach($data as $k=>$v){
+                if(strpos($k,'-')===false)
+                    continue;
+                $keys = explode('-',$k);
+                $newNameTmpArr = ['title'=>$v];
+                if($v!=$tmp_name[$keys[0]])
+                    Db::name('web_static')->where("name", $keys[0])->save($newNameTmpArr);//变更新此项
+            }
+            //看内容是否有变化:
+            foreach($data as $k=>$v){
+                if(strpos($k,'_')!==false)
+                    continue;
+                $newArr = ['content'=>$v];
+                if($v!=$temp[$k])
+                    Db::name('web_static')->where("name", $k)->save($newArr);//变更新此项
+            }
+            //看图片是否有变化：
+            foreach($result as $val){
+                if(empty($val['img']))
+                    continue;
+                $tmp[$val['name']] = $val['img'];
+            }
+            foreach($data as $k=>$v){
+                if(strpos($k,'_')===false)
+                    continue;
+                $keys = explode('_',$k);
+                $newTmpArr = ['img'=>$v];
+                if($v!=$tmp[$keys[0]])
+                    Db::name('web_static')->where("name", $keys[0])->save($newTmpArr);//变更新此项
+            }
             $this->ajaxReturn(['status'=>1,'msg'=>'操作成功','result'=>'']);
         }else{
-            $info = Db::name('web_static')->where('id',2)->find();
-            $this->assign('info',$info);
+            $info = Db::name('web_static')->where('id',2)->whereOr('pid',2)->select();
+            $title = $content = $img = [];
+            foreach($info as $v){
+                $title[$v['name']] = $v['title'];
+                $content[$v['name']] = $v['content'];
+                $img[$v['name']] = $v['img'];
+            }
+            $this->assign('img',$img);
+            $this->assign('content',$content);
+            $this->assign('title',$title);
             $this->initEditor(); // 编辑器
             return $this->fetch('_vip');
         }
